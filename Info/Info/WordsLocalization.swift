8ecf9035aa6d6a6527a83eb63f6a203f14ec4df4
Maksim Polinkovskii <max.polinkovsky@gmail.com>
2021-08-06 19:44:13 +0200

    sa
    Init

---
+++ Info/Info/WordsLocalization.swift
@@ -0,0 +1,16 @@
+//
+//  WordsLocalization.swift
+//  Info
+//
+//  Created by Max Polinkovsky on 23.07.21.
+//
+
+import Foundation
+
+let parentName = "Parent's name".localized()
+let nomore5 = "No more than 5 children".localized()
+let header = "Add childs".localized()
+let addName = "Add name".localized()
+let addAge = "Add age".localized()
+let delete = "Delete".localized()
+
